606a2d9ce413580ece43e58806e58f864b00db44
Egor Korotchenko <ekorotchenko@dataart.com>
2015-11-21 14:48:56 +0100

    draft of workers

--- src/main/java/com/util/dbloader/workers/BulkInsertWorker.java
+++ src/main/java/com/util/dbloader/workers/BulkInsertWorker.java
@@ -1,9 +1,15 @@
 package com.util.dbloader.workers;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import com.util.dbloader.connections.ConnectionDescriptor;
+import com.util.dbloader.model.Metadata;
 import com.util.dbloader.model.RecordCache;
+import com.util.dbloader.model.statements.PreparedInsert;
 
 public class BulkInsertWorker implements Runnable {
 
@@ -13,19 +19,42 @@ public class BulkInsertWorker implements Runnable {
 	private final LinkedBlockingQueue<RecordCache> cacheQueue;
 	private final int bulkSize;
 	
-	public BulkInsertWorker(ConnectionDescriptor connectionDescriptor, String tableName, String schemaName,
-			LinkedBlockingQueue<RecordCache> cacheQueue, int bulkSize) {
+	public BulkInsertWorker(ConnectionDescriptor connectionDescriptor, Metadata md,
+			LinkedBlockingQueue<RecordCache> cacheQueue, int bulkSize) throws SQLException {
 		this.connectionDescriptor = connectionDescriptor;
-		this.tableName = tableName;
-		this.schemaName = schemaName;
+		this.tableName = md.getTableName(1);
+		this.schemaName = md.getSchemaName(1);
 		this.cacheQueue = cacheQueue;
 		this.bulkSize = bulkSize;
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		Connection connection = null;
+		try {
+			connection = connectionDescriptor.createConnection();
+			List<RecordCache> cacheList = new ArrayList<RecordCache>();
+			while (cacheQueue.isEmpty()) {
+				int k = bulkSize;
+				cacheList.clear();
+				while(k-- > 0 && !cacheQueue.isEmpty()) {
+					cacheList.add(cacheQueue.poll());
+				}
+				try {
+					pushSlice(connection, cacheList);
+				} catch (SQLException e1) {
+					System.err.printf("sql exception while writing to table (%s) of schema (%s)%n", tableName, schemaName);
+				}
+			}
+		} catch (SQLException e) {
+			System.err.println("failed to open connection to DB, worker exits");
+			e.printStackTrace();
+			return;
+		}
+	}
+	
+	private void pushSlice(Connection connection, List<RecordCache> cacheList) throws SQLException {
+		connection.prepareStatement(new PreparedInsert(null).getNamedInsert());
 	}
 
 }
--- src/main/java/com/util/dbloader/workers/PartitionSelectWorker.java
+++ src/main/java/com/util/dbloader/workers/PartitionSelectWorker.java
@@ -1,9 +1,13 @@
 package com.util.dbloader.workers;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import com.util.dbloader.connections.ConnectionDescriptor;
 import com.util.dbloader.model.RecordCache;
+import com.util.dbloader.model.statements.SelectFromTable;
 
 public class PartitionSelectWorker implements Runnable {
 	
@@ -24,7 +28,34 @@ public class PartitionSelectWorker implements Runnable {
 
 	@Override
 	public void run() {
-
+		Connection connection = null;
+		try {
+			connection = connectionDescriptor.createConnection();
+			while(!partitionQueue.isEmpty()) {
+				String partition = partitionQueue.poll();
+				System.out.printf("starting read new partition %s from table %s.%s", partition, schemaName, tableName);
+				try {
+					ResultSet rs = connection.createStatement().executeQuery(
+							new SelectFromTable(tableName, schemaName, partition).getQuery());
+					cacheQueue.put(new RecordCache(rs));
+				} catch (SQLException e1) {
+					System.err.printf("sql exception while reading partition (%s) of table (%s) of schema (%s)%n", partition, tableName, schemaName);
+				} catch (InterruptedException e) {
+					System.err.println("interrupted operation of adding item to cache queue");
+					e.printStackTrace();
+				}
+			}
+		} catch (SQLException e1) {
+			System.err.println("failed to open connection to DB, worker exits");
+			e1.printStackTrace();
+			return;
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) { /* ignore */ }
+			}
+		}
 	}
 
 }

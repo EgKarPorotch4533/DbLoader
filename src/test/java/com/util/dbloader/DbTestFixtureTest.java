8426915e455fe5ab03f40b647af3161048779570
Egor Korotchenko <ekorotchenko@dataart.com>
2015-11-22 06:22:46 +0100

    tests

--- src/test/java/com/util/dbloader/DbTestFixtureTest.java
+++ src/test/java/com/util/dbloader/DbTestFixtureTest.java
@@ -91,4 +91,36 @@ public class DbTestFixtureTest extends DbTestFixture {
 		super.cleanupAndDeleteDb("testCreateDb");
 	}
 
+	@Test
+	public void testCreateDb_CreateTableWithSchema() throws ClassNotFoundException, SQLException {
+		Connection conn = super.createDb("testCreateDb1");
+		Assert.assertNotNull(conn);
+		Assert.assertTrue(new File(super.classResourcesDir, "testCreateDb1.tmp").exists());
+		Assert.assertTrue(new File(super.classResourcesDir, "testCreateDb1.script").exists());
+		Assert.assertTrue(new File(super.classResourcesDir, "testCreateDb1.log").exists());
+		Assert.assertTrue(new File(super.classResourcesDir, "testCreateDb1.properties").exists());
+		Statement st = conn.createStatement();
+		st.executeUpdate("CREATE SCHEMA SCH AUTHORIZATION DBA");
+		st.close();
+		st = conn.createStatement();
+		st.executeUpdate("CREATE TABLE SCH.FOO (ID INTEGER, NAME VARCHAR(30))");
+		st.close();
+		st = conn.createStatement();
+		st.executeUpdate("INSERT INTO SCH.FOO (ID, NAME) VALUES (1, 'James')");
+		st.close();
+		st = conn.createStatement();
+		st.executeUpdate("INSERT INTO SCH.FOO (ID, NAME) VALUES (2, 'Barbara')");
+		st.close();
+		st = conn.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM SCH.FOO");
+		int counter = 0;
+		while (rs.next()) {
+			System.out.printf("%d %s%n", rs.getInt(1), rs.getString(2));
+			counter++;
+		}
+		Assert.assertEquals(2, counter);
+		conn.close();
+		super.cleanupAndDeleteDb("testCreateDb1");
+	}
+
 }

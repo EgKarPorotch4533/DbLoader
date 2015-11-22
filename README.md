08fbf5ed9712f6c7db21f495b142b60193e16898
Egor Korotchenko <ekorotchenko@dataart.com>
2015-11-22 17:59:53 +0100

    logical error of reader fixed

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 Everything is raw so far, but still you can take a look on idea. Stage of initial testing.
 Every suggestion & critics are welcome
 
-yegor.korotchenko@gmail.com
--- src/main/java/com/util/dbloader/workers/PartitionSelectWorker.java
+++ src/main/java/com/util/dbloader/workers/PartitionSelectWorker.java
@@ -33,15 +33,19 @@ public class PartitionSelectWorker implements Runnable {
 			connection = connectionDescriptor.createConnection();
 			while(!partitionQueue.isEmpty()) {
 				String partition = partitionQueue.poll();
-				System.out.printf("[%d] starting read new partition %s from table %s.%s",
-						Thread.currentThread().getId(), partition, schemaName, tableName);
+				final String query = new SelectFromTable(tableName, schemaName, partition).getQuery();
+				System.out.printf("[%d] starting read new partition %s from table %s.%s [%s]%n",
+						Thread.currentThread().getId(), partition, schemaName, tableName, query);
 				try {
 					ResultSet rs = connection.createStatement().executeQuery(
-							new SelectFromTable(tableName, schemaName, partition).getQuery());
-					cacheQueue.put(new RecordCache(rs));
+							query);
+					while (rs.next()) {
+						cacheQueue.put(new RecordCache(rs));
+					}
 				} catch (SQLException e1) {
 					System.err.printf("[%d] sql exception while reading partition (%s) of table (%s) of schema (%s)%n",
 							Thread.currentThread().getId(), partition, tableName, schemaName);
+					e1.printStackTrace();
 				} catch (InterruptedException e) {
 					System.err.printf("[%d] interrupted operation of adding item to cache queue%n", Thread.currentThread().getId());
 					e.printStackTrace();
